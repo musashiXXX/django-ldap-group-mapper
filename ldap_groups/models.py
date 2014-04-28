f5f2a428b2897d790e3bd151f41e8f8ee4c78f1c
Charles Hamilton <musashi@nefaria.com>
2014-04-28 15:52:31 -0400

    Minor cleanup

--- ldap_groups/models.py
+++ ldap_groups/models.py
@@ -6,7 +6,6 @@ from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from django_auth_ldap.backend import LDAPBackend
 from django_auth_ldap.backend import populate_user
-logger = logging.getLogger(__name__)
 
 # Note: These config options are required, at a minimum
 server_uri = settings.AUTH_LDAP_SERVER_URI
