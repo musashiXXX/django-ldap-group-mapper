5f5d34a8444a685e399f6fc6e019f0b6e58282be
Charles Hamilton <musashi@nefaria.com>
2014-05-01 15:56:29 -0400

    Added download_url to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import find_packages, setup
 
-setup(name = 'django-ldap-groups',
-      version = '0.1.0',
-      author = 'Charles Hamilton',
-      author_email = 'musashi@nefaria.com',
-      packages = find_packages(exclude = ['*.tests',
-                                          '*.tests.*',
-                                          'tests.*',
-                                          'tests']),
-      url = 'https://github.com/musashiXXX/django-ldap-groups',
-      download_url = '',
-      license = 'LICENSE.txt',
-      description = 'Django LDAP Group Mapper',
-      long_description = open('README.md').read(),
-      zip_safe = False,
-      install_requires = [
-        'django-auth-ldap >= 1.2.0',
-        'python-ldap >= 2.0',
-        'Django >= 1.3'
-      ])
+setup(
+  name = 'django-ldap-groups',
+  version = '0.1.0',
+  author = 'Charles Hamilton',
+  author_email = 'musashi@nefaria.com',
+  packages = find_packages(exclude = [
+    '*.tests', '*.tests.*','tests.*','tests']),
+  url = 'https://github.com/musashiXXX/django-ldap-groups',
+  download_url = 'https://github.com/musashiXXX/django-ldap-groups/tarball/0.1.0',
+  license = 'LICENSE.txt',
+  description = 'Django LDAP Group Mapper',
+  long_description = open('README.md').read(),
+  zip_safe = False,
+  install_requires = [
+    'django-auth-ldap >= 1.2.0',
+    'python-ldap >= 2.0',
+    'Django >= 1.3']
+)
