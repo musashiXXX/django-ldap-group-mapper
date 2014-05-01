eb68b8114b6db6c59540e618d0d688410069c1fa
Charles Hamilton <musashi@nefaria.com>
2014-05-01 17:28:24 -0400

    Repository needed to be renamed to eliminate confusion because a similar project, django-ldap-groups already existed on PyPi and google code.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ long_description = 'A simple LDAP group -> Django group mapper, for use with' \
                    + ' django-auth-ldap.'
 
 setup(
-  name = 'django-ldap-groups',
+  name = 'django-ldap-group-mapper',
   version = '0.1.0',
   author = 'Charles Hamilton',
   author_email = 'musashi@nefaria.com',
   packages = find_packages(exclude = [
     '*.tests', '*.tests.*','tests.*','tests']),
-  url = 'https://github.com/musashiXXX/django-ldap-groups',
-  download_url = 'https://github.com/musashiXXX/django-ldap-groups/tarball/0.1.0',
+  url = 'https://github.com/musashiXXX/django-ldap-group-mapper',
+  download_url = 'https://github.com/musashiXXX/django-ldap-group-mapper/tarball/0.1.0',
   keywords = ['django-auth-ldap', 'django', 'ldap'],
   license = 'LICENSE.txt',
   description = 'Django LDAP Group Mapper',
