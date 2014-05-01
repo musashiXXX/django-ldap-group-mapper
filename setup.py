fb8dd2466599d368534bf14c5ff6c91fcd0a408f
Charles Hamilton <musashi@nefaria.com>
2014-05-01 16:44:06 -0400

    Still tinkering with setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+long_description = 'A simple LDAP group -> Django group mapper, for use with' \
+                   + 'django-auth-ldap.'
+
 setup(
   name = 'django-ldap-groups',
   version = '0.1.0',
@@ -12,7 +15,7 @@ setup(
   keywords = ['django-auth-ldap', 'django', 'ldap'],
   license = 'LICENSE.txt',
   description = 'Django LDAP Group Mapper',
-  long_description = open('README.md').read(),
+  long_description = long_description,
   zip_safe = False,
   install_requires = [
     'django-auth-ldap >= 1.2.0',
